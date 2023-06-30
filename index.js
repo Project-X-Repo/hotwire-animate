f457877b1f3fb40878f288d5f0cf2d8b90ee82d8
Anton Zaharia <zaharia_anton@yahoo.com>
2023-06-30 07:34:56 +0100

    moved hotwire animate JS in a new repo

---
+++ index.js
@@ -0,0 +1,62 @@
+const animateCssClass = 'animate__animated'
+
+// On page load
+document.addEventListener('turbo:load', () => {
+  Array.from(document.querySelectorAll('[data-hw-animate]')).forEach((element) => {
+    element.classList.add(animateCssClass, buildAnimateClass(element.dataset.hwAnimate))
+    if (element.dataset.hwAnimateDuration) {
+      element.classList.add(buildAnimateClass(element.dataset.hwAnimateDuration))
+    }
+  })
+})
+
+// On template render
+document.addEventListener('turbo:before-stream-render', (event) => {
+  // Animate in
+  if (event.target.firstElementChild instanceof HTMLTemplateElement) {
+    const element = event.target.templateElement.content.firstElementChild
+    const animationName = element.dataset.hwAnimateIn
+
+    if (animationName) {
+      animateIn(element, animationName)
+    }
+  }
+
+  // Animate out
+  const elementToRemove = document.getElementById(event.target.target)
+  if (elementToRemove) {
+    const exitAnimation = elementToRemove.dataset.hwAnimateOut
+    if (exitAnimation) {
+      event.preventDefault()
+      let callback = () => event.target.performAction()
+      animateOut(elementToRemove, exitAnimation, callback)
+    }
+  }
+})
+
+export function animateIn(element, animationName) {
+  element.classList.add(animateCssClass, buildAnimateClass(animationName))
+}
+
+export function animateOut(element, animationName, callback) {
+  resetAnimationClasses(element)
+  element.classList.add(animateCssClass, buildAnimateClass(animationName))
+
+  element.addEventListener('animationend', function () {
+    callback()
+  })
+}
+
+export function resetAnimationClasses(element) {
+  // remove all classes that begin with 'animate__' that aren't 'animate__animated'
+  const classList = Array.from(element.classList)
+  const newClassList = classList.filter((e) => e === 'animate__animated' || !e.startsWith('animate__'))
+  element.classList = newClassList.join(' ')
+}
+
+function buildAnimateClass(animationName) {
+  return `animate__${animationName}`
+}
+
+// Animate.css class name format
+// animate__animated animate__[name]
